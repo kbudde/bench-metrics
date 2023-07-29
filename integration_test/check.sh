be6ecf07abdbce9ac98d3b4bb9ce667c4ead2352
Kris Budde <kris@budd.ee>
2023-07-29 21:00:46 +0200

    ✅ added prometheus integration test

---
+++ integration_test/check.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+docker network create local
+
+docker run -d -p 9090:9090 --network local --name prom prom/prometheus
+
+docker run -d -p 9898:9898 --network local --name benchem \
+  -e BENCH_URL="http://prom:9090" \
+  -e BENCH_QUERIES_up="count(up)" \
+  kbudde/bench-metrics:latest
+
+curl http://localhost:9898 > metrics.txt
+
+docker rm -f prom
+docker rm -f benchem
+docker network rm local
+
+grep 'benchem_prom_timeseries{query="up"} 1' metrics.txt || exit 1
+grep 'benchem_prometheus_request_duration_seconds_bucket{query="up",le="5"}' metrics.txt || exit 2
+grep 'process_cpu_seconds_total' metrics.txt || exit 3
+
